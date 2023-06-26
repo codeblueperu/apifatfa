7aa2d2d34aa478758baf116f21f63d6d9142a160
unknown <capoajedrez.29@gmail.com>
2023-06-26 09:03:00 -0500

    OneVersion

--- src/main/java/com/fatfa/model/controller/TransferenciaController.java
+++ src/main/java/com/fatfa/model/controller/TransferenciaController.java
@@ -1,20 +1,19 @@
 package com.fatfa.model.controller;
 
 import java.text.ParseException;
-import java.util.Date;
-
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.fatfa.model.entity.TransferenciaModel;
+import com.fatfa.model.service.IDeclaradosService;
 import com.fatfa.model.service.ITransferenciaService;
 import com.fatfa.utils.Constantes;
 
@@ -25,6 +24,9 @@ public class TransferenciaController {
 	@Autowired
 	private ITransferenciaService srvTransferencias;
 	
+	@Autowired
+	private IDeclaradosService srvDelcrados;
+	
 	@GetMapping("/listatransferencia")
 	public ResponseEntity<?> onListaTranferencia(@RequestParam("idEmpresa") int idEmpresa,
 			@RequestParam("estado") String estado, @RequestParam("fecha")String fecha) throws ParseException {
@@ -34,8 +36,16 @@ public class TransferenciaController {
 	
 	
 	@PostMapping("/savetransferencia")
-	public ResponseEntity<?> onAgregarTransferencia(@Valid @RequestBody TransferenciaModel datos){		
-		return ResponseEntity.ok(srvTransferencias.srvAgregarTransferencia(datos));
+	public ResponseEntity<?> onAgregarTransferencia(@RequestPart(name = "datos") TransferenciaModel datos, @RequestParam (name = "file", required = false) MultipartFile file){
+		if(file==null) {
+			return ResponseEntity.ok(srvTransferencias.srvAgregarTransferencia(datos));
+		}else {
+			String nombre = file.getOriginalFilename();
+			datos.setComprobante(nombre);
+			srvDelcrados.saveFile("transferencia//",file);
+			return ResponseEntity.ok(srvTransferencias.srvAgregarTransferencia(datos));
+		}
+		
 	}
 	
 }
